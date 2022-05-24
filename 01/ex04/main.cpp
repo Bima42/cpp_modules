b3bd9d5fd03d74c9e01e91b41ca4e4a668d12a81
Bima42 <tanpau42@gmail.com>
2022-05-24 20:00:27 +0200

    ex04 done

---
+++ 01/ex04/main.cpp
@@ -0,0 +1,63 @@
+#include "main.hpp"
+
+int	checkArgs(std::string &arg, std::string &s1, std::string &s2)
+{
+	if (arg.empty() || s1.empty() || s2.empty())
+	{
+		std::cout << "Error: an empty string was sent" << std::endl;
+		return (0);
+	}
+	return (1);	
+}
+
+int	replaceString(std::string arg, std::string s1, std::string s2)
+{
+	std::ifstream file;
+	std::ofstream newFile;
+	std::string	nameFile;
+	std::string	tmp;
+
+	if (!checkArgs(arg, s1, s2))
+		return (0);
+
+	file.open(arg);
+	if (file.fail())
+	{
+		std::cerr << "Error: " << strerror(errno);
+		return (0);
+	}
+
+	nameFile = arg;
+	nameFile.append(".replace", 8);
+
+	newFile.open(nameFile);
+
+	while (std::getline(file, tmp))
+	{
+		if (tmp == s1)
+			newFile << s2 << std::endl;
+		else
+			newFile << tmp << std::endl;
+	}
+
+	file.close();
+	newFile.close();
+
+	return (1);
+}
+
+int	main (int argc, char **argv)
+{
+	if (argc != 4)
+	{
+		std::cout << "Error: Wrong arguments" << std::endl;
+		std::cout << "1: Filename | 2: string | 3: string" << std::endl;
+		return (1);
+	}
+	else
+	{
+		if (!(replaceString(argv[1], argv[2], argv[3])))
+			return (1);
+	}
+	return (0);
+}
---
+++ 01/ex04/main.hpp
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+# define MAIN_H
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int	checkArgs(std::string &arg, std::string &s1, std::string &s2);
+int	replaceString(std::string arg, std::string s1, std::string s2);
+
+#endif
