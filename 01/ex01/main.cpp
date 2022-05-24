2dc6f00d98b4598357ea77591786f7b5af938d96
bimasword <tanpau42@gmail.com>
2022-05-24 14:47:26 +0200

    leaks ok

--- 01/ex01/main.cpp
+++ 01/ex01/main.cpp
@@ -3,8 +3,12 @@
 int main( void )
 {
 	ZombieHorde	horde;
+	ZombieHorde	horde1;
 
 	horde.horde = horde.zombieHorde(-9, "Dirty");
 	horde.announce();
+
+	horde1.horde = horde1.zombieHorde(10, "Dirty");
+	horde1.announce();
 	return (0);
 }
