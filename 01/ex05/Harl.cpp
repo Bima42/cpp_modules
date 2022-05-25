8e2e9dcb8402ed3cc59f5b8f1540b0906f10b1c6
Bima42 <tanpau42@gmail.com>
2022-05-25 10:35:01 +0200

    ex05 done

---
+++ 01/ex05/Harl.cpp
@@ -0,0 +1,62 @@
+#include "Harl.hpp"
+
+Harl::Harl ( void )
+{
+	return ;
+}
+
+Harl::~Harl ( void )
+{
+	return ;
+}
+
+void Harl::debug ( void )
+{
+	std::cout << "I love having extra bacon "
+		"for my 7XL-double-cheese-triple-pickle-specialketchup burger. "
+		"I really do!" << std::endl;
+}
+
+void Harl::info ( void )
+{
+	std::cout << "I cannot believe adding extra bacon costs more money. "
+		"You didn’t put enough bacon in my burger! "
+		"If you did, I wouldn’t be asking for more!" << std::endl;
+}
+
+void Harl::warning ( void )
+{
+	std::cout << "I think I deserve to have some extra bacon for free. "
+		"I’ve been coming for years "
+		"whereas you started working here since last month." << std::endl;
+}
+
+void Harl::error ( void )
+{
+	std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
+}
+
+void Harl::complain ( std::string level )
+{
+	void (Harl::*display[]) (void) = {
+		&Harl::debug,
+		&Harl::info,
+		&Harl::warning,
+		&Harl::error,
+	}; // array of pointer func
+
+	std::string lvl[] = {
+		"debug",
+		"info",
+		"warning",
+		"error",
+	}; // string array
+
+	int i = 0;
+	while (i < 4)
+	{
+		if (lvl[i] == level)
+			(this->*display[i])();
+		i++;
+	}
+}
