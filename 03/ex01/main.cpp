63d1d45e48d338bfff8ea9e6203923d99a0bb2b5
Bima42 <tanpau42@gmail.com>
2022-05-28 17:24:14 +0200

    add setter ex00 and ex01

---
+++ 03/ex01/main.cpp
@@ -0,0 +1,28 @@
+#include "ClapTrap.hpp"
+
+int main( void )
+{
+	ClapTrap test("Dude");
+
+	test.attack("Buddy");
+	test.attack("Buddy");
+	test.attack("Buddy");
+	test.takeDamage(9);
+	test.beRepaired(15);
+	test.takeDamage(12);
+	test.beRepaired(40);
+	test.takeDamage(1);
+	test.takeDamage(12);
+	test.takeDamage(1);
+	test.attack("Buddy");
+	test.attack("Buddy");
+	test.attack("Buddy");
+	test.attack("Buddy");
+	test.attack("Buddy");
+	test.attack("Buddy");
+	test.attack("Buddy");
+	test.attack("Buddy");
+	test.attack("Buddy");
+
+	return (0);
+}
