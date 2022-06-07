3928054485ba819b81717dfb234546ed8ca8155c
Bima42 <tanpau42@gmail.com>
2022-06-07 11:36:32 +0200

    ex01 done

---
+++ 06/ex01/Data.cpp
@@ -0,0 +1,30 @@
+#include "Data.hpp"
+
+Data::Data ( void ): _content() {}
+
+Data::Data ( const Data &copy ): _content(copy.getContent()) {}
+
+Data::~Data ( void ) {}
+
+Data &Data::operator = ( const Data &right ) {
+	(void)right;
+	return (*this);
+}
+
+std::string Data::getContent ( void ) const {
+	return (this->_content);
+}
+
+void Data::setContent ( const std::string content ) {
+	this->_content = content;
+}
+
+uintptr_t serialize ( Data *ptr ) {
+	uintptr_t ret = reinterpret_cast<uintptr_t>(ptr);
+	return (ret);
+}
+
+Data *deserialize ( uintptr_t raw ) {
+	Data *ret = reinterpret_cast<Data *>(raw);
+	return (ret);
+}
---
+++ 06/ex01/Data.hpp
@@ -0,0 +1,28 @@
+#ifndef DATA_H
+# define DATA_H
+
+# include <iostream>
+# include <string>
+# include <stdint.h>
+
+class Data {
+	
+	public:
+		Data ( void );
+		Data ( int x, int y );
+		Data ( const Data &copy );
+		virtual ~Data ( void );
+
+		Data &operator=( const Data &right );
+
+		std::string getContent ( void ) const;
+		void setContent ( const std::string content );
+
+	private:
+		std::string _content;
+};
+
+uintptr_t serialize ( Data *ptr );
+Data *deserialize ( uintptr_t raw );
+
+#endif
